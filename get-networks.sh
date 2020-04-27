efe5a2173459daad9242e8107c7793c0e2f5b435
ramcho3855 <ramcho3855@gmail.com>
2020-04-27 17:42:26 +0530

    changed netwrok download link

--- get-networks.sh
+++ get-networks.sh
@@ -3,7 +3,7 @@
 set -e
 
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1SnXi_oPDPzDVYQS6LNnh5qrGLoac3Ntb' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1SnXi_oPDPzDVYQS6LNnh5qrGLoac3Ntb" -O data.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1K9Q044bCB7-O7LtrY7UrSzF2V4lmDS8D' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1K9Q044bCB7-O7LtrY7UrSzF2V4lmDS8D" -O data.zip && rm -rf /tmp/cookies.txt
 unzip data.zip
 rm data.zip
 
