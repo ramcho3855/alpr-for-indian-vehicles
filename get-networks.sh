5ee1dcf6c704f208e5c4c1842e147738ab176a58
Ramavatar Choudhary <37657984+ramcho3855@users.noreply.github.com>
2020-06-01 14:38:54 +0530

    updated getnetworks.sh

--- get-networks.sh
+++ get-networks.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-!wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1pwkkDjcS7uVsvC0wucIgzXLzj0o2bDl7' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1pwkkDjcS7uVsvC0wucIgzXLzj0o2bDl7" -O data.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1pwkkDjcS7uVsvC0wucIgzXLzj0o2bDl7' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1pwkkDjcS7uVsvC0wucIgzXLzj0o2bDl7" -O data.zip && rm -rf /tmp/cookies.txt
 unzip data.zip
 rm data.zip
 
